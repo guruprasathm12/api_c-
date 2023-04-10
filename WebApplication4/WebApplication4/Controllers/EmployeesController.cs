975404e56a93b073aa8bcc9e053932d6da4c9e64
vishnu <vishnubharathi44@gmail.com>
2023-04-10 15:39:50 +0530

    first commit

---
+++ WebApplication4/WebApplication4/Controllers/EmployeesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private sampleEntities db = new sampleEntities();
+
+        // GET: Employees
+        public async Task<ActionResult> Index()
+        {
+            var emp=await db.Employees.ToListAsync();
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var emp = await db.Employees.FindAsync(id);
+
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,MobileNo,EmailId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.Id = Guid.NewGuid();
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return Json("created successfully", JsonRequestBehavior.AllowGet);
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var emp = await db.Employees.FindAsync(id);
+            
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,MobileNo,EmailId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var emp = await db.Employees.FindAsync(id);
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
